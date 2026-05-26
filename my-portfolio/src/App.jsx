30fe7e1d5092499f686c1b46bf46b289dbdcda50
shahariarsabbir <217337202+shahariarsabbir@users.noreply.github.com>
2026-05-26 19:55:12 +0600

    First commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,137 @@
+import React, { Component, useState } from 'react'
+import './App.css'
+import me from './assets/me.jpg'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import Education from './Education'
+import Work from './Work'
+import Programing from './Programing'
+import Project from './Project'
+
+export default function App() {
+	const [work, setwork] = useState(false)
+	const [programing, setprograming] = useState(false)
+	const [project, setproject] = useState(false)
+	const [education, seteducation] = useState(false)
+
+	return (
+		<>
+			<div className="App">
+				<div className="hero-section py-5 px-10">
+					<nav className="d-flex flex-row justify-content-between">
+						<span className="brand-name ">SM</span>
+						<div className="d-flex flex-row">
+							<span className="mx-4 nav-items">Home</span>
+							<span className="mx-4 nav-items">Contact</span>
+							<span className="mx-4 nav-items">About Me</span>
+							<span className="mx-4 nav-items">Resume</span>
+							<span className="mx-4 nav-items">Testimonial</span>
+						</div>
+					</nav>
+					<div className="hero-section2 justify-content-center row">
+						<div className="col-lg-6 col-md-6 col-sm-12">
+							<div className="d-flex justify-content-center flex-column iamtextsection">
+								<span className="iamtext">
+									Hello, I'm
+									<span className="nameiamtext"> SM SHAHARIAR SABBIR </span>
+								</span>
+								<span className="iamenthusiastics py-2">
+									Enthusiastics Dev 😎
+								</span>
+								<span className="iamtextsubheading">
+									knack of building application frontend and backend operations
+								</span>
+								<div className="d-flex flex-row justify-content-center mt-5">
+									<button className="btn-hire-me">Hire Me</button>
+									<button className="btn-resume-me">Get Resume</button>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-6 col-md-6 col-sm-12">
+							<div className="justify-content-center d-flex Anim">
+								<img src={me} alt="" className="img-fluid custom-img" />
+							</div>
+						</div>
+					</div>
+					<br />
+					<br />
+				</div>
+				{/* about me section */}
+
+				<div className="about-me-section aboutme-inside my-5">
+					<div className="d-flex flex-column">
+						<span className="aboutme-text">About Me</span>
+						<span className="whyme-text">Why Choose Me</span>
+						<div className="row justify-content-center shadow-lg my-5">
+							<div className="col-lg-6 col-md-6 d-flex col-sm-12 align-items-center justify-content-center mt-2">
+								<DotLottieReact
+									src="https://lottie.host/a1acd914-32a5-4511-b142-c1c5065aa16e/dXFijnLTf3.lottie"
+									loop
+									autoplay
+									style={{ width: '700px', height: '300px' }}
+								/>
+							</div>
+							<div className="col-lg-6 col-md-6 d-flex col-sm-12 align-items-center justify-content-center">
+								<span className="some-text-about-me p-4 pe-3">
+									I enjoy building responsive and user-friendly applications
+									while continuously learning new tools and frameworks. I love
+									solving problems, exploring innovative ideas, and improving my
+									development skills every day.
+									<span className="few-highlights"></span>
+									<br />
+									<span className="few-text"> Few highlights</span>
+									<span className="few-list">
+										<ul>
+											<li>Full-Stack Web Development (MERN)</li>
+											<li>Responsive UI/UX Design</li>
+											<li>Machine Learning Basics</li>
+											<li>Cyber Security Enthusiast</li>
+											<li>Docker & AWS Fundamentals</li>
+											<li>Problem Solving & DSA</li>
+										</ul>
+									</span>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="resume-outer-section d-flex flex-column">
+					<span className="aboutme-text">Resume</span>
+					<span className="whyme-text">My Formal Bio</span>
+					<div
+						className="resume-new-section row"
+						style={{ width: '70%', marginInline: 'auto' }}
+					>
+						<div className="col-lg-4 col-md-4 resume-left-section d-flex flex-row">
+							<div className="d-flex flex-column bg-new text-dark">
+								<span>
+									<i class="bi bi-mortarboard-fill"></i>
+								</span>
+								<span>
+									<i class="bi bi-briefcase-fill"></i>
+								</span>
+								<span>
+									<i class="bi bi-code-slash"></i>
+								</span>
+								<span>
+									<i class="bi bi-list-task"></i>
+								</span>
+								<div className="d-flex flex-column">
+									<span className="resume-option-items">work</span>
+									<span className="resume-option-items">Educatio</span>
+									<span className="resume-option-items">programing</span>
+									<span className="resume-option-items">Projects</span>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-8 col-md-8">
+							{education === true && <Education />}
+							{programing === true && <Programing />}
+							{work === true && <Work />}
+							{project === true && <Project />}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
---
+++ my-portfolio/src/Programing.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Programing = () => {
+	return <div>programing</div>
+}
+
+export default Programing
